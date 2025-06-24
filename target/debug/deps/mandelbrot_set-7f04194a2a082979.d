32789fe49b5fed0a129526d6df5ee5e4918ae6cc
gosfi <samlef12@outlook.com>
2025-06-24 15:02:45 -0400

    test2

--- target/debug/deps/mandelbrot_set-7f04194a2a082979.d
+++ target/debug/deps/mandelbrot_set-7f04194a2a082979.d
@@ -1,5 +1,5 @@
-/run/media/samuel/504121f9-e58c-4243-868c-f77571b20947/HDD/dev/Rust/RustInActionProjects/Part1/mandelbrot_set/target/debug/deps/libmandelbrot_set-7f04194a2a082979.rmeta: src/main.rs
+/run/media/samuel/504121f9-e58c-4243-868c-f77571b20947/HDD/dev/Rust/RustInActionProjects/Part1/Mandelbrot_set/target/debug/deps/libmandelbrot_set-7f04194a2a082979.rmeta: src/main.rs
 
-/run/media/samuel/504121f9-e58c-4243-868c-f77571b20947/HDD/dev/Rust/RustInActionProjects/Part1/mandelbrot_set/target/debug/deps/mandelbrot_set-7f04194a2a082979.d: src/main.rs
+/run/media/samuel/504121f9-e58c-4243-868c-f77571b20947/HDD/dev/Rust/RustInActionProjects/Part1/Mandelbrot_set/target/debug/deps/mandelbrot_set-7f04194a2a082979.d: src/main.rs
 
 src/main.rs:
